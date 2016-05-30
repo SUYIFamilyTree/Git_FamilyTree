147943000828b57448807ad9bb6435ff7343bdf4
amonest <598028978@qq.com>
2016-05-30 13:41:09 +0800

    查询界面已完成

--- FamilyTreeFIR/FamilyTree/查询/controller/SearchFamilyTreeViewController.m
+++ FamilyTreeFIR/FamilyTree/查询/controller/SearchFamilyTreeViewController.m
@@ -7,6 +7,14 @@
 //
 
 #import "SearchFamilyTreeViewController.h"
+#import "SearchTableViewCell.h"
+
+@interface SearchFamilyTreeViewController()<UITableViewDelegate,UITableViewDataSource>
+/** 搜索栏*/
+@property (nonatomic, strong) UIView *searchView;
+
+
+@end
 
 @implementation SearchFamilyTreeViewController
 
@@ -18,12 +26,11 @@
     [self initNavi];
     //搜索栏
     [self initSearchBar];
-//    UISearchBar *seachBar = [[UISearchBar alloc]initWithFrame:CGRectMake(12, NavigationBar_Height+StatusBar_Height+8, Screen_width-24, 30)];
-//    seachBar.barTintColor = LH_RGBCOLOR(236, 236, 236);
-//    seachBar.placeholder = @"输入关键词";
-//    [self.view addSubview:seachBar];
+    //添加表格
+    [self initTableView];
+    
 }
-
+//导航栏
 -(void)initNavi{
     UIView *topView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, Screen_width, 64)];
     topView.backgroundColor = LH_RGBCOLOR(75, 88, 91);
@@ -45,12 +52,12 @@
     [rightButton addTarget:self action:@selector(clickRightButton) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:rightButton];
 }
-
+//搜索栏
 -(void)initSearchBar{
-    UIView *searchView = [[UIView alloc]initWithFrame:CGRectMake(12, NavigationBar_Height+StatusBar_Height+8, Screen_width-24, 30)];
-    searchView.layer.cornerRadius = 30/2.0f;
-    searchView.backgroundColor = [UIColor whiteColor];
-    [self.view addSubview:searchView];
+    _searchView = [[UIView alloc]initWithFrame:CGRectMake(12, NavigationBar_Height+StatusBar_Height+8, Screen_width-24, 30)];
+    _searchView.layer.cornerRadius = 30/2.0f;
+    _searchView.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:_searchView];
     UITextField *searchTF = [[UITextField alloc]initWithFrame:CGRectMake(25, NavigationBar_Height+StatusBar_Height+8, Screen_width-50, 30)];
     searchTF.backgroundColor = [UIColor whiteColor];
     searchTF.placeholder = @"输入关键词";
@@ -62,6 +69,13 @@
     UITapGestureRecognizer *searchTap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(clickSearch)];
     [searchImageView addGestureRecognizer:searchTap];
     
+}
+//添加表格
+-(void)initTableView{
+    UITableView *tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, CGRectYH(_searchView)+10, Screen_width, 120)];
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    [self.view addSubview:tableView];
 }
 //返回按钮
 -(void)clickLeftButton{
@@ -77,6 +91,39 @@
 -(void)clickSearch{
     MYLog(@"搜索开始");
 }
+
+#pragma mark - UITableViewDelegate,UITableViewDataSource
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 2;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    SearchTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"searchCell"];
+    if (!cell) {
+        cell = [[SearchTableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"searchCell"];
+    }
+    if (indexPath.row == 0) {
+        cell.imageV.image = MImage(@"find");
+        cell.titleLB.text = @"同支宗亲";
+        cell.detailLB.text = @"查找同支宗亲";
+    }else{
+        cell.imageV.image = MImage(@"adress");
+        cell.titleLB.text = @"附近宗亲";
+        cell.detailLB.text = @"查找附近宗亲";
+    }
+    
+    return cell;
+    
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 60;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     [self.view endEditing:YES];
 }
