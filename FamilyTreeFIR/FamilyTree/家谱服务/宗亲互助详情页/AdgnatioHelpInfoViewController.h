2c9587feaac45f13a84da24f33bc51cd53b928d9
amonest <598028978@qq.com>
2016-08-02 13:39:34 +0800

    我的互助页面功能完成

---
+++ FamilyTreeFIR/FamilyTree/家谱服务/宗亲互助详情页/AdgnatioHelpInfoViewController.h
@@ -0,0 +1,14 @@
+//
+//  AdgnatioHelpInfoViewController.h
+//  FamilyTree
+//
+//  Created by 姚珉 on 16/8/2.
+//  Copyright © 2016年 王子豪. All rights reserved.
+//
+
+#import "BaseViewController.h"
+#import "MyHelpModel.h"
+
+@interface AdgnatioHelpInfoViewController : BaseViewController
+
+@end
