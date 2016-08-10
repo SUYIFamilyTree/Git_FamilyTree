85276918991ab4e378b847d9a34868da8c19f2f2
forSubaru <subaru@hanshijiandemac-mini.local>
2016-08-10 15:08:55 +0800

    支付宝，等后台数据

--- FamilyTreeFIR/FamilyTree/AppDelegate.m
+++ FamilyTreeFIR/FamilyTree/AppDelegate.m
@@ -13,6 +13,7 @@
 #import "GuideViewController.h"
 
 #import "WXApi.h"
+#import <AlipaySDK/AlipaySDK.h>
 @interface AppDelegate ()<WXApiDelegate>
 
 @end
@@ -85,8 +86,39 @@
 -(BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url{
     return [WXApi handleOpenURL:url delegate:self];
 }
--(BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation{
-    return [WXApi handleOpenURL:url delegate:self];
+
+#pragma mark *** huidiao ***
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation {
+    
+    if ([sourceApplication isEqualToString:@"com.tencent.xin"]) {
+        //微信支付回调
+        return [WXApi handleOpenURL:url delegate:self];
+    }
+    
+    if ([url.host isEqualToString:@"safepay"]) {
+        //跳转支付宝钱包进行支付，处理支付结果
+        [[AlipaySDK defaultService] processOrderWithPaymentResult:url standbyCallback:^(NSDictionary *resultDic) {
+            NSLog(@"result = %@",resultDic);
+        }];
+    }
+    return YES;
+    
+
+}
+
+// NOTE: 9.0以后使用新API接口
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString*, id> *)options
+{
+    if ([url.host isEqualToString:@"safepay"]) {
+        //跳转支付宝钱包进行支付，处理支付结果
+        [[AlipaySDK defaultService] processOrderWithPaymentResult:url standbyCallback:^(NSDictionary *resultDic) {
+            NSLog(@"result = %@",resultDic);
+        }];
+    }
+    return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
--- FamilyTreeFIR/FamilyTree/登录界面/登录/LoginViewController.m
+++ FamilyTreeFIR/FamilyTree/登录界面/登录/LoginViewController.m
@@ -11,6 +11,8 @@
 #import "LoginModel.h"
 #import "RootTabBarViewController.h"
 #import "WXApi.h"
+#import <AlipaySDK/AlipaySDK.h>
+#import "AliPayOrder.h"
 #define ReGistView_height 180
 #define AnimationsTime 0.4f
 @interface LoginViewController ()<LoginViewDelegate,ToRegisViewDelegate>
@@ -180,10 +182,14 @@
             NSLog(@"weixin");
             
             if (![WXApi isWXAppInstalled]) {
-                [Tools showAlertViewcontrollerWithTarGet:self Message:@"未安装微信" complete:nil];
+                [Tools showAlertViewcontrollerWithTarGet:self Message:@"未安装微信" complete:^(BOOL sure) {
+                    
+                }];
                 return;
             }else if(![WXApi isWXAppSupportApi]){
-                [Tools showAlertViewcontrollerWithTarGet:self Message:@"此版本不支持支付" complete:nil];
+                [Tools showAlertViewcontrollerWithTarGet:self Message:@"此版本不支持支付" complete:^(BOOL sure) {
+                    
+                }];
                 return;
             }
             
@@ -233,6 +239,83 @@
         case 2:
         {
             NSLog(@"weibo");
+            /*
+             *在调用支付宝之前，需要我们将相关订单参数发送至我们的后台服务器，由后台服务器进行签名等处理，并返回客户端所有相关参数，
+             客户端直接使用参数调起支付宝支付
+             */
+            
+            /*
+             *商户的唯一的parnter和seller。
+             *签约后，支付宝会为每个商户分配一个唯一的 parnter 和 seller。
+             */
+            
+            /*============================================================================*/
+            /*=======================需要填写商户app申请的===================================*/
+            /*============================================================================*/
+            NSString *partner = @"";
+            NSString *seller = @"";
+            NSString *privateKey = @"";
+            //这些重要信息不在客户端保存
+            /*============================================================================*/
+            /*============================================================================*/
+            /*============================================================================*/
+            
+            //partner和seller获取失败,提示
+            if ([partner length] == 0 ||
+                [seller length] == 0 ||
+                [privateKey length] == 0)
+            {
+                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"提示"
+                                                                message:@"缺少partner或者seller或者私钥。"
+                                                               delegate:self
+                                                      cancelButtonTitle:@"确定"
+                                                      otherButtonTitles:nil];
+                [alert show];
+                return;
+            }
+            
+            /*
+             *生成订单信息及签名
+             */
+            //将商品信息赋予AlixPayOrder的成员变量
+            AliPayOrder *order = [[AliPayOrder alloc] init];
+            order.partner = partner;
+            order.sellerID = seller;
+            order.outTradeNO = @"123123"; //订单ID（由商家自行制定）
+            order.subject = @"ourMakise"; //商品标题
+            order.body = @"elpsycongrol"; //商品描述
+            order.totalFee = @"0.01"; //商品价格
+            order.notifyURL =  @"http://www.xxx.com"; //回调URL
+            
+            order.service = @"mobile.securitypay.pay";
+            order.paymentType = @"1";
+            order.inputCharset = @"utf-8";
+            order.itBPay = @"30m";
+            order.showURL = @"m.alipay.com";
+            
+            //应用注册scheme,在AlixPayDemo-Info.plist定义URL types
+            NSString *appScheme = @"alisdkdemo";
+            
+            //将商品信息拼接成字符串
+            NSString *orderSpec = [order description];
+            NSLog(@"orderSpec = %@",orderSpec);
+            
+            //获取私钥并将商户信息签名,外部商户可以根据情况存放私钥和签名,只需要遵循RSA签名规范,并将签名字符串base64编码和UrlEncode
+            //    id<DataSigner> signer = CreateRSADataSigner(privateKey);
+            //    NSString *signedString = [signer signString:orderSpec];
+            NSString *signedString = @"xxxxx_sign";
+            
+            //将签名成功字符串格式化为订单字符串,请严格按照该格式
+            NSString *orderString = nil;
+            if (signedString != nil) {
+                orderString = [NSString stringWithFormat:@"%@&sign=\"%@\"&sign_type=\"%@\"",
+                               orderSpec, signedString, @"RSA"];
+                
+                [[AlipaySDK defaultService] payOrder:orderString fromScheme:appScheme callback:^(NSDictionary *resultDic) {
+                    NSLog(@"reslut = %@",resultDic);
+                }];
+            }
+
            
         }
             break;
